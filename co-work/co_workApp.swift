39ab56c88b0d5d7406c8e61516b2e0ae8ce1469f
hongyi <liuhongyi790@gmail.com>
2022-10-24 16:30:22 +0800

    Initial Commit

---
+++ co-work/co_workApp.swift
@@ -0,0 +1,17 @@
+//
+//  co_workApp.swift
+//  co-work
+//
+//  Created by liufamily on 2022/10/24.
+//
+
+import SwiftUI
+
+@main
+struct co_workApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
